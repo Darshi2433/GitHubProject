15a3881ba12d1c25c0e8ea874c81b515232f8739
kunj patel <darshipatel335@gmail.com>
2020-10-15 16:56:18 -0400

    main project

---
+++ src/mainPage/FirstClass.java
@@ -0,0 +1,20 @@
+package mainPage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("this is a main method");
+
+	}
+	
+	public static void second() {
+		System.out.println("this is a second method");
+	}
+	
+	public void third() {
+		System.out.println("this is a non static method");
+	}
+
+}
