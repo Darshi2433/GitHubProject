6e7bc0c11622d49eaf2afa3bc28fa4146ce78fb0
kunj patel <darshipatel335@gmail.com>
2020-10-15 17:05:16 -0400

    second

--- src/mainPage/FirstClass.java
+++ src/mainPage/FirstClass.java
@@ -1,12 +1,19 @@
 package mainPage;
 
+
+
 public class FirstClass {
 
 	public static void main(String[] args) {
 
 
 		System.out.println("this is a main method");
-
+		FirstClass f1 = new FirstClass();
+		SecondClass s2 = new SecondClass();
+        s2.fourth();
+        FirstClass.second();
+        f1.third();
+        
 	}
 	
 	public static void second() {
@@ -14,7 +21,7 @@ public class FirstClass {
 	}
 	
 	public void third() {
-		System.out.println("this is a non static method");
+		System.out.println("this is a non static third method");
 	}
 
 }
---
+++ src/mainPage/SecondClass.java
@@ -0,0 +1,15 @@
+package mainPage;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is a second class");
+
+	}
+	
+	public void fourth() {
+		System.out.println("this is a fourth class");
+	}
+
+}
